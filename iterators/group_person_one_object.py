3ea9e4567d6f35851cd648724f53f86f384771bb
Daniel Barty <nixspam@web.de>
2024-09-04 16:27:27 +0200

    Add iterator example

---
+++ iterators/group_person_one_object.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+class Group:
+    def __init__(self):
+        self.members = []
+        
+    def __add__(self, person):
+        if type(person).__name__ == "Person":
+            self.members.append(person)
+            
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        try:
+            return self.members.pop(0)
+        except:
+            raise StopIteration()
+            
+class Person:
+    def __init__(self, name):
+        self.name = name
+        
+    def __str__(self):
+        return self.name
+
+group = Group()
+group + Person("Daniel")  # Calls Group::__add__()
+group + Person("Helga")
+
+# Group::__iter__() is called and returns the iterator for group
+# Group::__next__() is called with each loop
+for member in group:
+    print(member)
