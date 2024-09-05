e12acb786bb652f66f76ee4afb5fe2d0e1a310a1
Daniel Barty <nixspam@web.de>
2024-09-05 08:55:04 +0200

    Delete comprehension directory

--- comprehension/dict/key_value.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-my_dict = {f"key{x}": x for x in range(10)}
-
-print(my_dict)
-print(my_dict["key5"])
--- comprehension/list/2d_with_xy_tuple.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-my_2D_list = [[(x, y) for x in range(10)] for y in range(10)]
-
-print(my_2D_list)
