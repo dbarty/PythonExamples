12b5427e007c6908e47eeeeeaa4d78dd2d243850
Daniel Barty <nixspam@web.de>
2024-09-05 10:26:40 +0200

    Add module example

---
+++ modules/mymodule.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def __str__(self):
+        return f"{self.name} ({self.age})"
