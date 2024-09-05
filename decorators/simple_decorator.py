52a10355a5dcd5028c738aa9b0f2adcc47625e52
Daniel Barty <nixspam@web.de>
2024-09-05 10:30:46 +0200

    Update simple_decorator.py

--- decorators/simple_decorator.py
+++ decorators/simple_decorator.py
@@ -3,8 +3,8 @@
 def my_decorator(func, before="(", after=")"):
     # Passing the args is important, if the function accepts arguments.
     # Mandatory when decorating methods (needs self)
-    def inner(*args): 
-        return before + func(*args) + after
+    def inner(*args, **kwargs): 
+        return before + func(*args, **kwargs) + after
     return inner
     
 @my_decorator
