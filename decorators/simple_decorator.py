f0ab6d18a72f006244c84c11f294e439ed967721
Daniel Barty <nixspam@web.de>
2024-09-06 08:52:10 +0200

    Update simple_decorator.py

--- decorators/simple_decorator.py
+++ decorators/simple_decorator.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
     
-def my_decorator(func, before="(", after=")"):
+def simple_decorator(func):
     # Passing the args is important, if the function accepts arguments.
     # Mandatory when decorating methods (needs self)
     def inner(*args, **kwargs): 
-        return before + func(*args, **kwargs) + after
+        return "(" + func(*args, **kwargs) + ")"
     return inner
     
-@my_decorator
+@simple_decorator
 def greeting(name="World"):
     return f"Hello {name}!"
     
