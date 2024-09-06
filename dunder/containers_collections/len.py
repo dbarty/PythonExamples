ac96bd059136dbc0d6239e981d75fe2189fbe67d
Daniel Barty <nixspam@web.de>
2024-09-06 10:52:30 +0200

    Create len.py

---
+++ dunder/containers_collections/len.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class Group:
+    def __init__(self):
+        self.members = []
+        
+    def __len__(self):
+        return len(self.members)
+    
+    def add(self, person):
+        if type(person).__name__ == "Person":
+            self.members.append(person)
+            
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+group = Group()
+group.add(Person("Daniel"))
+group.add(Person("Helga"))
+
+print("Number members:", len(group))
